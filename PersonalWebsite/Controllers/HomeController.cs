51c8e0b4c5e534a9cbf5c82813939383061a3843
Gearde Geldenhuys <Gerade@warpdevelopment.co.za>
2017-09-06 16:19:27 +0200

    initial commit

---
+++ PersonalWebsite/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Name = "Gerade";
+        public Array Interests = new[] {
+                                         ".NET", "C#", "Javascript", "Azure", "Sitefinity", "Xamarin", "Web", "Mobile" };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
